f6a7b8495e0c4ae87fd4d4f312e64689540b090e
Negar Saleh <107615239+negarslh@users.noreply.github.com>
2024-05-24 23:00:15 +0330

    Update black_hole.py
    debugging

--- Assignment_29/2_Black Hole/black_hole.py
+++ Assignment_29/2_Black Hole/black_hole.py
@@ -1,38 +1,56 @@
 import os
-import cv2
 import numpy as np
-import random
+from PIL import Image
+import cv2
 
-folder_path_1 = '2_Black Hole/input/black_hole/1'
-folder_path_2 = '2_Black Hole/input/black_hole/2'
-folder_path_3 = '2_Black Hole/input/black_hole/3'
-folder_path_4 = '2_Black Hole/input/black_hole/4'
-
-image_1 = []
-image_2 = []
-image_3 = []
-image_4 = []
-
-for image_path in [folder_path_1 , folder_path_2 , folder_path_3 , folder_path_4] :
-    image = cv2.imread("2_Black Hole/input/black_hole/"+image_path).astype(np.float32)
-
-    image_1.append(image)
-    image_2.append(image)
-    image_3.append(image)
-    image_4.append(image)
-
-result = np.zeros(image.shape)
-
-for image in image_1 :
-    result += image
-
-for image in image_2 :
-    result += image
-
-for image in image_3 :
-    result += image
-
-for image in image_4 :
-    result += image
-
+def calculate_mean_image(directory):
+    image_arrays = []
     
+    for filename in os.listdir(directory):
+        if filename.endswith('.jpg'):
+            image_path = os.path.join(directory, filename)
+            image = Image.open(image_path).convert('RGB')
+            image_array = np.array(image)
+            image_arrays.append(image_array)
+    
+    if not image_arrays:
+        raise ValueError("No images found in the directory.")
+    
+    mean_image_array = np.mean(image_arrays, axis=0).astype(np.uint8)
+    
+    mean_image = Image.fromarray(mean_image_array)
+    
+    return mean_image
+
+def process_multiple_directories(base_directory, subdirectories):
+    all_mean_images_cv2 = []
+    
+    for subdirectory in subdirectories:
+        directory_path = os.path.join(base_directory, subdirectory)
+        try:
+            mean_image = calculate_mean_image(directory_path)
+            mean_image_cv2 = cv2.cvtColor(np.array(mean_image), cv2.COLOR_RGB2BGR)
+            cv2.waitKey(0) 
+            cv2.destroyAllWindows()
+            
+            all_mean_images_cv2.append(mean_image_cv2)
+        except ValueError as e:
+            print(f"Skipping {subdirectory}: {e}")
+    
+    return all_mean_images_cv2
+
+base_directory = '2_Black Hole/input/black_hole'
+subdirectories = ['1', '2', '3', '4']
+
+mean_images_list = process_multiple_directories(base_directory, subdirectories)
+
+combined_image_up = np.concatenate(mean_images_list[:2], axis=1)
+combined_image_down = np.concatenate(mean_images_list[2:], axis=1)
+
+black_hole = np.concatenate((combined_image_up, combined_image_down), axis=0)
+
+cv2.imwrite('2_Black Hole/output/black_hole.jpg', black_hole)
+
+cv2.imshow("black_hole", black_hole)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

7b5032e9d1ff0ca4316d45c01a54ee0df9f6d77e
Negar Saleh <107615239+negarslh@users.noreply.github.com>
2024-05-24 23:06:22 +0330

    Update secret_text.py
    debugging

--- Assignment_29/4_Find the secret text/secret_text.py
+++ Assignment_29/4_Find the secret text/secret_text.py
@@ -7,8 +7,9 @@ image_2 = cv2.imread('4_Find the secret text/input/image_2.png')
 image_1 = image_1.astype(np.float32)
 image_2 = image_2.astype(np.float32)
 
-result = cv2.subtract(image_1, image_2)
+result = cv2.subtract(image_2 , image_1)
 result = result.astype(np.uint8)
+result = 255 - result
 
 cv2.imwrite('4_Find the secret text/output/result.png', result)
 
